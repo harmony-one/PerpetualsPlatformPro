967df8d66afda3245366d74e46e1207f09a4121b
SchickMarkus <markuschick@web.de>
2021-05-29 17:59:38 +0200

    Add tests

--- packages/hardhat/test/main.js
+++ packages/hardhat/test/main.js
@@ -2,44 +2,95 @@ const { expect } = require("chai");
 const { waffle } = require("hardhat");
 const { utils } = require("ethers");
 
-use(solidity);
 
 describe("Perpetual Protocol", function () {
 
   // test data. Use BigNumber to avoid overflow
-  const fullAmount = utils.parseEther("1000"); // 1000
-  const approvedAmount = utils.parseEther("200"); // 100
-  const initialContractBalance = utils.parseEther("50"); // 100
+  const supply = utils.parseEther("10000"); 
+  const vUSDreserve = utils.parseEther("1000000"); 
+  const vXAUreserve = utils.parseEther("10000"); 
+  const investAmount = utils.parseEther("100")
+  const leverage = 5;
 
   let usdc;
   let perpetual;
-
+  let ownerAmount;
 
   beforeEach('Set up', async () => {
 
-    [owner, saver, debtor, ...addrs] = await ethers.getSigners();
+    [owner, user1, user2, ...addrs] = await ethers.getSigners();
 
-    const YourContract = await ethers.getContractFactory("YourContract");
+    const USDC = await ethers.getContractFactory("USDC");
+    usdc = await USDC.connect(owner).deploy(supply);
 
-    myContract = await YourContract.deploy();
-
+    const Perpetual = await ethers.getContractFactory("Perpetual");
+    perpetual = await Perpetual.deploy(usdc.address, vUSDreserve, vXAUreserve, leverage );
 
 
   });
 
-  describe("Can deposit money", function () {
-    it("Should allow Owner to start the redeeming phase", async function () {
-      // contract call
-      await swapcontract.start_redeeming();
-      const exchange_rate_end = await swapcontract.exchange_rate_end();
-
-      // should be sucessful
-      expect(exchange_rate_end).not.be.null;
-      expect(exchange_rate_end).to.equal(await swapcontract.getEUROPrice());
-      const principal_balance = await swapcontract.total_pool_prinicipal();
-      // expect(principal_balance).to.equal(initialContractBalance.add(approvedAmount));
-
+  describe("Can be deployed", function () {
+    it("Should given tokens with deployment ", async function () {
+      ownerAmount = await usdc.balanceOf(owner.address);
+      expect(ownerAmount).to.be.equal(supply);
+    });
+    it("Should given public variables ", async function () {
+      const USDC = await perpetual.USDC();
+      expect(USDC).to.be.equal(usdc.address);
     });
   });
- 
+
+  describe("Can deposit/withdraw USDC", function () {
+    it("Should give allowance to perpetual", async function () {
+      await usdc.connect(owner).approve(perpetual.address, investAmount);
+      const allowance = await usdc.allowance(owner.address, perpetual.address);
+      expect(allowance).to.be.equal(investAmount);
+
+    });
+    it("Should deposit USDC", async function () {
+      await usdc.connect(owner).approve(perpetual.address, investAmount);
+      const allowance = await usdc.allowance(owner.address, perpetual.address);
+      expect(allowance).to.be.equal(investAmount);
+
+      await expect(perpetual.deposit(investAmount))
+        .to.emit(perpetual, 'Deposit')
+        .withArgs(investAmount, owner.address);
+
+    });
+    it("Should withdraw USDC", async function () {
+      await usdc.connect(owner).approve(perpetual.address, investAmount);
+      await perpetual.deposit(investAmount)
+
+      await expect(perpetual.withdraw(investAmount))
+      .to.emit(perpetual, 'Withdraw')
+      .withArgs(investAmount, owner.address);
+    
+    });
+  });
+
+  describe("Can trade vXAU", function () {
+    it("Should buy vXAU", async function () {
+      await usdc.connect(owner).approve(perpetual.address, investAmount);
+      await perpetual.deposit(investAmount);
+
+      /*await expect(perpetual.MintLongXAU(investAmount))
+      .to.emit(perpetual, 'LongXAUminted')
+      .withArgs(utils.parseEther("49.75"), owner.address);*/
+
+    });
+    it("Should sell vXAU", async function () {
+      await usdc.connect(owner).approve(perpetual.address, investAmount);
+      await perpetual.deposit(investAmount);
+
+      await perpetual.MintLongXAU(investAmount);
+      const XAUamount = await perpetual.vXAUlong(owner.address);
+
+
+      await expect(perpetual.RedeemLongXAU(XAUamount))
+      .to.emit(perpetual, 'LongXAUredeemed')
+      .withArgs(utils.parseEther("500"), owner.address);
+
+    });
+
+  });
 });        
